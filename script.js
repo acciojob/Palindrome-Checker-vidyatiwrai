b0bd6d61b8fadf380d772e19ba8ad3223dd5becb
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-23 10:16:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,28 @@
 
 function palindrome(str){
 
+	// program to check if the string is palindrome or not
+
+
+    // find the length of a string
+    const len = str.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (str[i] !== str[len - 1 - i]) {
+            return 'false';
+        }
+    }
+    return 'true';
 }
+
+// take input
+const string = prompt('Enter a string: ');
+
+// call the function
+const value = checkPalindrome(string);
+
+console.log(value);
 module.exports = palindrome
