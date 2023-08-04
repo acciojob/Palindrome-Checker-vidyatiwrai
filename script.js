53632f28a5b413ab0f2cd56e05934b120e46ba9b
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-08-04 16:58:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function palindrome{
-	for(let i=0;i<s.length;i++){
-	for(let j=s.length;j>=i;j--){
-		if(s.charAt(i)!=s.charAt(j)){
-			return "false";
-		}else{
-			
-		}
-	}
-		return "true";
-	}
+function palindrome(str){
+ str=str.toLowerCase();
+    let i=0;
+    let j=str.length-1;
+    while(i<j){
+        if(str.charAt(i)==str.charAt(j)){
+            i++;
+            j--;
+        }else{
+           return false;
+        }
+    }
+    return true;
 }
+module.exports = palindrome
